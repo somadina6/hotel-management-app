126eb0f2fa917072f1b9586c870935ec8a22660d
somadina6 <somadina6@gmail.com>
2023-12-24 00:26:53 -0400

    developed signup form frontend

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -10,5 +10,24 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
       ? JSON.parse(localStorage.getItem("hotel-theme")!)
       : false;
 
-  const [darkTheme, setDarkTheme] = useState<boolean>();
+  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+  const [renderComponent, setRenderComponent] = useState(false);
+
+  useEffect(() => {
+    setRenderComponent(true);
+  }, []);
+
+  if (!renderComponent) return <></>;
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className="dark:text-white dark:bg-black text-[#1e1e1e]">
+          {children}
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
 };
+
+export default ThemeProvider;
