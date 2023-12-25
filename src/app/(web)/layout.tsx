8ad543e7e423e25ae3a0b077ccf7387b2482a8fc
somadina6 <somadina6@gmail.com>
2023-12-25 01:32:00 -0400

    changes

---
+++ src/app/(web)/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Header from "@/components/Header/Header";
+import "./globals.css";
+import Footer from "@/components/Footer/Footer";
+import ThemeProvider from "@/components/ThemeProvider/ThemeProvider";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Hotel Management App",
+    template: "%s",
+  },
+  description: "Hotel App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <ThemeProvider>
+          <main className="font-normal">
+            <Header />
+            {children}
+            <Footer />
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
