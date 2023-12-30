0b5d2b4d85996428766847e0e501995dee6f9927
somadina6 <somadina6@gmail.com>
2023-12-30 19:20:33 -0400

    Add authentication functionality and schema types

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import user from "./user";
+import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import review from "./review";
+import verificationToken from "./verificationToken";
+
+export const schemaTypes = [
+  user,
+  account,
+  booking,
+  hotelRoom,
+  review,
+  verificationToken,
+];
---
+++ schemas/user.ts
@@ -0,0 +1,48 @@
+import { defineField } from "sanity";
+
+const user = {
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    defineField({
+      name: "isAdmin",
+      title: "Is Admin",
+      type: "boolean",
+      description: "Check if user is admin",
+      initialValue: false,
+      validation: (Rule) => Rule.required(),
+      // readOnly: true,
+      // hidden: true,
+    }),
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: "name of the user",
+      readOnly: true,
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "url",
+    }),
+
+    defineField({
+      name: "password",
+      type: "string",
+      hidden: true,
+    }),
+
+    defineField({
+      name: "about",
+      title: "About",
+      type: "text",
+      description: "A brief des about user",
+    }),
+  ],
+};
+
+export default user;
